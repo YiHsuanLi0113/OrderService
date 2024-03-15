6978339816384cace98ac1b331ffeef9cd0cd37d
ysl19950113 <ysl19950113@gmail.com>
2024-03-15 14:00:07 +0800

    6_5_1_Backend_Orderservice_加入連線字串、注入DB和CORS服務

--- Program.cs
+++ Program.cs
@@ -1,6 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using OrderService.Data;
+using OrderService.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+//using Microsoft.EntityFrameworkCore;
+//using Microsoft.EntityFrameworkCore;
+//using OrderService.Data;
+//using OrderService.Models;
+builder.Services.AddDbContext<StoreContext>(options =>options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<IProductRepo, ProductRepo>();
+builder.Services.AddScoped<CartService>();
+builder.Services.AddScoped<CheckoutService>();
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -8,6 +21,13 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+var allowCors = builder.Configuration["AppConfig:Cors"].Split(",");
+            app.UseCors(options =>
+            options.WithOrigins(allowCors)
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
